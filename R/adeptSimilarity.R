5ed2216ad1742c23c931945af9c94945e237b676
‘Marta <marta.karass@gmail.com>
2019-01-04 11:58:46 -0500

    add comments to adeptSimilarity function

--- R/adeptSimilarity.R
+++ R/adeptSimilarity.R
@@ -1,19 +1,36 @@
 
 
 
-#' Title
+#' Compute ADEPT similarity matrix
 #'
-#' @param x
-#' @param template.scaled
-#' @param similarity.measure
+#' Compute ADEPT similarity matrix between time-series \code{x} and a collection
+#' of rescaled versions of empirical pattern(s).
 #'
-#' @return
+#' @param x A numeric vector; time-series \code{x}.
+#' @param template.scaled A list of rescaled versions of empirical pattern(s).
+#' Number of elements in the list corresponds to number of unique pattern scale
+#' parameters considered. Each element of \code{template.scaled} is a list
+#' itself and contains numeric vector(s) where each vector corresponds to
+#' one of possibly multiple distinct patterns considered.
+#' @param similarity.measure Character value; one of the following:
+#' \itemize{
+#'   \item "cov",
+#'   \item "cor".
+#' }
+#'
+#' @return A numeric matrix with similarity values; number of columns
+#' corresponds to vector length of time-series \code{x}. Number of rows
+#' corresponds different pattern scale values considered (equivalently:
+#' length of \code{template.scaled} list). Each row of the matrix consists
+#' of vector of running similarity statistic (correlation, covariance etc.)
+#' between  \code{x} and a pattern template(s) rescaled to matrix row-specific
+#' particular scale parameter. Precisely, a vector with the highest similarity
+#' result at each time point, over possibly multiple pattern templates, is
+#' returned.
 #'
 #' @import runstats
 #'
-#' @export
-#'
-#' @examples
+#' @noRd
 #'
 adeptSimilarity <- function(x,
                             template.scaled,
@@ -24,19 +41,20 @@ adeptSimilarity <- function(x,
                          "cor" = RunningCor)
 
   ## Outer lapply: iterate over template scales considered:
-  ## each lapply iteration "fills one row" of final similarity matrix
+  ## each lapply iteration fills one row of an output similarity matrix
   similarity.list <- lapply(template.scaled, function(template.scaled.i){
 
-    ## Outer lapply: iterate over distinct templates considered (fixed template scale)
+    ## Inner lapply: iterate over distinct pattern templates considered
+    ## (fixed template scale); a vector with the highest similarity result
+    ## at each time point, over possibly multiple pattern templates, is returned
     runstat.func.out0 <- lapply(template.scaled.i, function(template.scaled.ik){
       do.call(runstat.func, list(x = x, y = template.scaled.ik))
     })
-
-    ## Compute and store highest result over possibly multiple templates
     do.call(pmax, runstat.func.out0)
+
   })
 
-  ## rbind into a matrix
+  ## rbind list elements (which are vectors) into a matrix
   similarity.mat <- do.call(rbind, similarity.list)
   return(similarity.mat)
 
