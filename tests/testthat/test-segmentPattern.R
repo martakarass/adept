b075a05897b9ddeefd5048b2e156f385bbc1e726
‘Marta <marta.karass@gmail.com>
2019-01-11 13:37:41 -0500

    Add basic tests for segmentPattern()

--- tests/testthat/test-segmentPattern.R
+++ tests/testthat/test-segmentPattern.R
@@ -496,10 +496,218 @@ test_that("Example 2(d): no noise in signal (no peak fine-tuning employed),
   expect_equal(res, res.exp)
 
   res <- out$sim_i
-  res.exp <- c(0.59186478601464, 0.540764650172252, 0.455919521310586, 0.728924948585804,
-               0.634803250987258, 0.352900628868909, 0.730662404365108, 0.442075511219409,
-               0.718248596674937, 0.698948050109923)
+  res.exp <- c(0.728924948585804, 0.634803250987258, 0.730662404365108, 0.718248596674937,
+               0.698948050109923)
   expect_equal(res, res.exp)
+})
+
+
+
+######################
+######################
+######################
+###   Examples 3   ###
+######################
+######################
+######################
+
+
+
+test_that("Example 3(a): add noise in signal generation,
+          pattern occurences of different length in singal generation", {
+
+  ## Generate signal and template
+  set.seed(1)
+  s.grid <- sample(60:120, size = 10)
+  templ <- cos(seq(0, 2 * pi, length.out = 200))
+
+  x <- numeric()
+  for (ss in s.grid){
+    templ0 <- approx(seq(0, 1, length.out = 200), templ, xout = seq(0, 1, length.out = ss))$y
+    if (length(x)>0){
+      x <- x[-length(x)]
+    }
+    x <- c(x, templ0)
+  }
+  x <- x + rnorm(length(x), sd = 0.3)
+
+  pattern.dur.seq <- seq(60, 100, by = 5)
+  out <- segmentPattern(x = x,
+                        x.fs = 1,
+                        template = template,
+                        pattern.dur.seq = pattern.dur.seq,
+                        similarity.measure = "cor")
+
+  res <- out$tau_i
+  res.exp <- c(4, 73, 162, 254, 358, 435, 545, 646, 752, 862)
+  expect_equal(res, res.exp)
+
+  res <- out$T_i
+  res.exp <- c(70, 90, 85, 100, 75, 100, 100, 100, 100, 60)
+  expect_equal(res, res.exp)
+
+  res <- out$sim_i
+  res.exp <- c(0.926291306033366, 0.943178533162363, 0.909156293953748, 0.905525014013764,
+               0.922523794573569, 0.878846405199884, 0.896401753440613, 0.908357873425458,
+               0.892508399360682, 0.932536508269999)
+  expect_equal(res, res.exp)
+})
+
+
+
+
+test_that("Example 3(b): add noise in signal generation,
+          pattern occurences of different length in singal generation", {
+
+  ## Generate signal and template
+  set.seed(1)
+  s.grid <- sample(60:120, size = 10)
+  templ <- cos(seq(0, 2 * pi, length.out = 200))
+
+  x <- numeric()
+  for (ss in s.grid){
+    templ0 <- approx(seq(0, 1, length.out = 200), templ, xout = seq(0, 1, length.out = ss))$y
+    if (length(x)>0){
+      x <- x[-length(x)]
+    }
+    x <- c(x, templ0)
+  }
+  x <- x + rnorm(length(x), sd = 0.3)
+
+
+
+  ## Without maxima detection
+  pattern.dur.seq <- seq(60, 100, by = 5)
+  out <- segmentPattern(x = x,
+                        x.fs = 1,
+                        template = template,
+                        pattern.dur.seq = pattern.dur.seq,
+                        similarity.measure = "cor")
+
+  res <- out$tau_i
+  res.exp <- c(4, 73, 162, 254, 358, 435, 545, 646, 752, 862)
+  expect_equal(res, res.exp)
+
+  res <- out$T_i
+  res.exp <- c(70, 90, 85, 100, 75, 100, 100, 100, 100, 60)
+  expect_equal(res, res.exp)
+
+  res <- out$sim_i
+  res.exp <- c(0.926291306033366, 0.943178533162363, 0.909156293953748, 0.905525014013764,
+               0.922523794573569, 0.878846405199884, 0.896401753440613, 0.908357873425458,
+               0.892508399360682, 0.932536508269999)
+  expect_equal(res, res.exp)
+
+
+
+  ## Use maxima detection (for comparison)
+  pattern.dur.seq <- seq(60, 100, by = 5)
+  out <- segmentPattern(x = x,
+                        x.fs = 1,
+                        template = template,
+                        pattern.dur.seq = pattern.dur.seq,
+                        similarity.measure = "cor",
+                        finetune = "maxima",
+                        finetune.maxima.ma.W = 30,
+                        finetune.maxima.nbh.W = 120)
+
+  res <- out$tau_i
+  res.exp <- c(15, 77, 160, 257, 356, 434, 544, 651, 750, 849)
+  expect_equal(res, res.exp)
+
+  res <- out$T_i
+  res.exp <- c(63, 84, 89, 100, 79, 100, 100, 97, 100, 60)
+  expect_equal(res, res.exp)
+
+  res <- out$sim_i
+  res.exp <- c(0.934008825205221, 0.943178533162363, 0.917627061535885, 0.905525014013764,
+               0.922523794573569, 0.878846405199884, 0.896401753440613, 0.908357873425458,
+               0.890287715999962, 0.932536508269999)
+  expect_equal(res, res.exp)
+
+
+
+  ## Use maxima detection, use thresholding on covariance
+  pattern.dur.seq <- seq(60, 100, by = 5)
+  out <- segmentPattern(x = x,
+                        x.fs = 1,
+                        template = template,
+                        pattern.dur.seq = pattern.dur.seq,
+                        similarity.measure.thresh = 0.7,
+                        similarity.measure = "cov",
+                        finetune = "maxima",
+                        finetune.maxima.ma.W = 30,
+                        finetune.maxima.nbh.W = 120)
+
+  res <- out$tau_i
+  res.exp <- c(15, 77, 160, 651, 849)
+  expect_equal(res, res.exp)
+
+  res <- out$T_i
+  res.exp <- c(63, 84, 89, 97, 60)
+  expect_equal(res, res.exp)
+
+  res <- out$sim_i
+  res.exp <- c(0.705378003362227, 0.735918052569151, 0.759884581289357, 0.727240717460776,
+               0.742750320506588)
+  expect_equal(res, res.exp)
+
+
+
+  ## Use maxima detection, use thresholding on covariance,
+  ## and add smoothing
+  pattern.dur.seq <- seq(60, 100, by = 5)
+  out <- segmentPattern(x = x,
+                        x.fs = 1,
+                        template = template,
+                        pattern.dur.seq = pattern.dur.seq,
+                        similarity.measure.thresh = -1,
+                        x.adept.ma.W = 30,
+                        similarity.measure = "cov",
+                        finetune = "maxima",
+                        finetune.maxima.ma.W = 30,
+                        finetune.maxima.nbh.W = 120)
+
+  res <- out$tau_i
+  res.exp <- c(15, 77, 160, 257, 356, 434, 544, 651, 751, 852)
+  expect_equal(res, res.exp)
+
+  res <- out$T_i
+  res.exp <- c(63, 84, 89, 100, 79, 100, 100, 97, 100, 60)
+  expect_equal(res, res.exp)
+
+  res <- out$sim_i
+  res.exp <- c(0.387425918617852, 0.59334391287926, 0.628645307416092, 0.564182215998019,
+               0.524567526202169, 0.573154721743202, 0.561415062231443, 0.610366994688926,
+               0.589176030015489, 0.33404216471104)
+  expect_equal(res, res.exp)
+
+
+  ## increase range of allowed pattern duration (pattern.dur.seq)
+  pattern.dur.seq <- seq(50, 150, by = 5)
+  out <- segmentPattern(x = x,
+                        x.fs = 1,
+                        template = template,
+                        pattern.dur.seq = pattern.dur.seq,
+                        similarity.measure = "cor",
+                        finetune = "maxima",
+                        finetune.maxima.ma.W = 30,
+                        finetune.maxima.nbh.W = 120)
+
+  res <- out$tau_i
+  res.exp <- c(15, 77, 160, 248, 356, 434, 537, 651, 747, 852)
+  expect_equal(res, res.exp)
+
+  res <- out$T_i
+  res.exp <- c(63, 84, 89, 109, 79, 104, 115, 97, 106, 57)
+  expect_equal(res, res.exp)
+
+  res <- out$sim_i
+  res.exp <- c(0.934008825205221, 0.943178533162363, 0.917627061535885, 0.905525014013764,
+               0.922523794573569, 0.878846405199884, 0.913586332840362, 0.902548497003548,
+               0.892508399360682, 0.932536508269999)
+  expect_equal(res, res.exp)
+
 })
 
 
