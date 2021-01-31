d7165add91e75b4674e8206ee6053a0c3f7824c9
Marta Karas <martakaras@Martas-MacBook-Pro.local>
2021-01-31 18:35:49 -0500

    change tests so they are hopefully <10 s now

--- tests/testthat/test-segmentWalking.R
+++ tests/testthat/test-segmentWalking.R
@@ -4,7 +4,7 @@ context("Testing segmentWalking function.")
 # define raw accelerometry data sample with walking activity
 xyz <-
   adeptdata::acc_walking_IU %>%
-  filter(loc_id == "left_wrist", subj_id == "id86237981") %>%
+  filter(loc_id == "left_wrist", subj_id == "id86237981", time_s < 70) %>%
   arrange(time_s) %>%
   select(v1 = x, v2 = y, v3 = z) %>%
   as.matrix()
@@ -26,21 +26,18 @@ test_that("Test that the result remains unchanged.", {
 
   # tau_i
   out_tmp <- as.numeric(table(diff(out$tau_i)))
-  out_tmp_exp <- c(1, 1, 2, 2, 4, 4, 1, 2, 1, 5, 5, 14, 29, 35, 33, 19, 13, 9,
-                 1, 1, 1, 1, 1, 3, 3, 2, 4, 1)
+  out_tmp_exp <- c(1, 1, 1, 8, 11, 10, 11, 15, 5, 3, 1)
   expect_equal(out_tmp, out_tmp_exp)
 
   # T_i
   out_tmp <- as.numeric(table(diff(out$T_i)))
-  out_tmp_exp <- c(1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 2, 1, 1, 1,
-                   2, 6, 10, 4, 22, 12, 21, 17, 21, 14, 11, 4, 3, 1, 3, 1, 3, 5,
-                   4, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2)
+  out_tmp_exp <- c(1, 1, 3, 4, 4, 12, 4, 8, 6, 10, 7, 4, 1, 1, 1)
   expect_equal(out_tmp, out_tmp_exp)
 
   # T_i
   out_tmp <- as.numeric(summary(out$sim_i))
-  out_tmp_exp <- c(0.447788834120344, 0.927338638590566, 0.950041534695681, 0.936063202907588,
-                   0.964910021616661, 0.9871113293843)
+  out_tmp_exp <- c(0.609683895990804, 0.937583597090984, 0.95016460341074, 0.936577027289593,
+                   0.959373815418658, 0.98301945400386)
   expect_equal(out_tmp, out_tmp_exp)
 })
 
@@ -54,21 +51,18 @@ test_that("Test that the result remains unchanged when using parallel.", {
 
   # tau_i
   out_tmp <- as.numeric(table(diff(out$tau_i)))
-  out_tmp_exp <- c(1, 1, 2, 2, 4, 4, 1, 2, 1, 5, 5, 14, 29, 35, 33, 19, 13, 9,
-                   1, 1, 1, 1, 1, 3, 3, 2, 4, 1)
+  out_tmp_exp <- c(1, 1, 1, 8, 11, 10, 11, 15, 5, 3, 1)
   expect_equal(out_tmp, out_tmp_exp)
 
   # T_i
   out_tmp <- as.numeric(table(diff(out$T_i)))
-  out_tmp_exp <- c(1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 2, 1, 1, 1,
-                   2, 6, 10, 4, 22, 12, 21, 17, 21, 14, 11, 4, 3, 1, 3, 1, 3, 5,
-                   4, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2)
+  out_tmp_exp <- c(1, 1, 3, 4, 4, 12, 4, 8, 6, 10, 7, 4, 1, 1, 1)
   expect_equal(out_tmp, out_tmp_exp)
 
   # T_i
   out_tmp <- as.numeric(summary(out$sim_i))
-  out_tmp_exp <- c(0.447788834120344, 0.927338638590566, 0.950041534695681, 0.936063202907588,
-                   0.964910021616661, 0.9871113293843)
+  out_tmp_exp <- c(0.609683895990804, 0.937583597090984, 0.95016460341074, 0.936577027289593,
+                   0.959373815418658, 0.98301945400386)
   expect_equal(out_tmp, out_tmp_exp)
 })
 
