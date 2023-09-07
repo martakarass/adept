109c9e674c7a34fcf2da07345d0d70592d79f526
muschellij2 <muschellij2@gmail.com>
2023-09-07 14:27:38 -0400

    removed old code

--- R/segmentPattern.R
+++ R/segmentPattern.R
@@ -352,49 +352,6 @@ segmentPattern <- function(x,
     mc.cores.val = mc.cores.val
   )
 
-  out.list <- parallel::mclapply(x.cut.seq, function(i){
-    ## Define current x part indices
-    idx.i <- i : min((i + x.cut.vl + x.cut.margin), length(x))
-    ## If we cannot fit the longest pattern, return NULL
-    if (length(idx.i) <= max(template.vl)) return(NULL)
-    ## Compute similarity matrix
-    similarity.mat.i <- similarityMatrix(x = x.smoothed[idx.i],
-                                         template.scaled = template.scaled,
-                                         similarity.measure = similarity.measure)
-    ## Compute template index matrix
-    if (compute.template.idx){
-      template.idx.mat.i <- templateIdxMatrix(x = x.smoothed[idx.i],
-                                              template.scaled = template.scaled,
-                                              similarity.measure = similarity.measure)
-    }
-    else
-    {
-      template.idx.mat.i <- NULL
-    }
-    ## Run max and tine procedure
-    out.df.i <- maxAndTune(x = x[idx.i],
-                           template.vl = template.vl,
-                           similarity.mat = similarity.mat.i,
-                           similarity.measure.thresh = similarity.measure.thresh,
-                           template.idx.mat = template.idx.mat.i,
-                           finetune = finetune,
-                           finetune.maxima.x = finetune.maxima.x[idx.i],
-                           finetune.maxima.nbh.vl = finetune.maxima.nbh.vl)
-
-    ## Shift \tau parameter according to which part of signal x we are currently working with
-    if (nrow(out.df.i) > 0){
-      out.df.i$tau_i <- out.df.i$tau_i + i - 1
-      return(out.df.i)
-
-    } else {
-      ## Return empty data frame
-      return(data.frame(tau_i = numeric(),
-                        T_i = numeric(),
-                        sim_i = numeric(),
-                        template_i = numeric()))
-    }
-  }, mc.cores = getOption("mc.cores", mc.cores.val))
-
   ## ---------------------------------------------------------------------------
   ## Clear up after possibly multiple stride occurrences
   out.df <- do.call("rbind", out.list)
